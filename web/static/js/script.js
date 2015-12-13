abba8591d9f386134215f5d0e78c9d2b636a75a0
kopringo <kopringo@gmail.com>
2015-12-14 00:20:10 +0100

    new layout

--- web/static/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$(".table-person-list tr").click(function(){
-		location.href = $(this).attr('data-href');
-		return false;
-	});
-});
--- web/views.py
+++
@@ -1,7 +0,0 @@
-# Create your views here.
-
-from GenealogyViewer.utils import render_to_response
-from models import *
-
-def home(request):
-    return render_to_response(request, 'web/index.html', {})
