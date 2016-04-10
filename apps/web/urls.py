32588c83178cc4fcc673a8ea801fd342b71649b0
kopringo <kopringo@gmail.com>
2016-04-10 23:56:28 +0200

    url fix

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -9,21 +9,18 @@ from views_trees import index, index_import
 
 urlpatterns = [
     
-    url(r'^(?P<tree_slug>.+)/$', home, name='home'),
+    url(r'^(?P<tree_slug>[\w-]+)/$', home, name='home'),
     
-    url(r'^(?P<tree_slug>.+)/person/list$',                 person_list,  name='person_list'),
-    url(r'^(?P<tree_slug>.+)/person/(?P<pid>.*)/$',                person_view,  name='person_view'),
+    url(r'^(?P<tree_slug>[\w-]+)/person/list$',                 person_list,  name='person_list'),
+    url(r'^(?P<tree_slug>[\w-]+)/person/(?P<pid>\d+)/$',                person_view,  name='person_view'),
     
-    url(r'^(?P<tree_slug>.+)/family/list$',                      family_list,  name='family_list'),
+    url(r'^(?P<tree_slug>[\w-]+)/family/list$',                      family_list,  name='family_list'),
     
-    url(r'^(?P<tree_slug>.+)/branch/list$',                      branch_list, name='branch_list'),
+    url(r'^(?P<tree_slug>[\w-]+)/branch/list$',                      branch_list, name='branch_list'),
     
-    url(r'^(?P<tree_slug>.+)/test$',                             test),
+    url(r'^(?P<tree_slug>[\w-]+)/test$',                             test),
     
-    url(r'^(?P<tree_slug>.+)/manager$',                           manager, name='manager'),
-    
-    # mechanizm przelaczania wersji jezykowej
-    #url(r'^lang/(?P<id>[plen]+)',   'views.lang',   name='lang'),
+    url(r'^(?P<tree_slug>[\w-]+)/manager$',                           manager, name='manager'),
     
     url(r'^$', index, name='index'),
     url(r'^import$', index_import, name='index_import'),
