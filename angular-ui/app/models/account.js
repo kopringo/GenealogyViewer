acd584841a5f379bf4d581c8574bdd034cd7165f
kopringo <kopringo@gmail.com>
2016-10-23 23:46:25 +0200

    models

---
+++ angular-ui/app/models/account.js
@@ -0,0 +1,12 @@
+"use strict";
+var Account = (function () {
+    function Account(id, name, power, alterEgo) {
+        this.id = id;
+        this.name = name;
+        this.power = power;
+        this.alterEgo = alterEgo;
+    }
+    return Account;
+}());
+exports.Account = Account;
+//# sourceMappingURL=account.js.map
