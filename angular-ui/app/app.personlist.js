10c4cdc37ec835c54e013a66721a0852fa3a8190
kopringo <kopringo@gmail.com>
2016-10-25 20:56:54 +0200

    .js .js.map files are auto generated

--- angular-ui/app/app.personlist.js
+++ angular-ui/app/app.personlist.js
@@ -11,11 +11,23 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppPersonList = (function () {
     function AppPersonList() {
+        this.mode = 'Observable';
     }
+    //constructor (private personService: PersonService) {}
+    AppPersonList.prototype.ngOnInit = function () { this.getList(); };
+    AppPersonList.prototype.getList = function () {
+        return [];
+        /*
+        this.personService.getList()
+                     .subscribe(
+                       person_list => this.person_list = person_list,
+                       error =>  this.errorMessage = <any>error);
+                       */
+    };
     AppPersonList = __decorate([
         core_1.Component({
-            selector: 'my-app',
-            template: "\n    <div class=\"row\">\n        <div class=\"col-md-6\">lista os\u00F3b</div>\n        <div class=\"col-md-6\">Tree</div>\n    </div>\n  "
+            selector: 'app-person-list',
+            template: "\n    <div class=\"row\">\n        <div class=\"col-md-6\">[{{mode}}]\n            <input type=\"text\" name=\"q\" class=\"form-control\" placeholder=\"Who are you looking for?\"/>\n            <br/>\n            \n            <table class=\"table table-condensed\">\n                <thead>\n                    <tr>\n                        <th>Name...</th>\n                        <th>Birth</th>\n                        <th>Dead</th>\n</tr>\n</thead>\n                <tbody>\n                <tr>\n                    <td><a routerLink=\"/person-list/1\" routerLinkActive=\"active\">Micha\u0142</a></td>\n                    <td></td>\n                    <td></td>\n                </tr>\n                <tr>\n                    <td><a routerLink=\"/person-list/2\" routerLinkActive=\"active\">Franek</a></td>\n                    <td></td>\n                    <td></td>\n                </tr>\n                \n                <template ngFor let-item [ngForOf]=\"person_list\" let-i=\"index\" [ngForTrackBy]=\"trackByFn\">\n                \t<tr>\n\t\t                <td><a routerLink=\"/person-list/{{person.id}}\" routerLinkActive=\"active\">{{person.fist_name}}</a></td>\n\t\t                <td></td>\n\t\t                <td></td>\n\t\t            </tr>\n                </template>\n                \n                </tbody>\n            </table>\n        \n        </div>\n        <div class=\"col-md-6\">Tree</div>\n    </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], AppPersonList);
